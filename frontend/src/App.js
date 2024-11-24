48e075dc0f678c4a9cb7419e5af8c4beccf40f94
Niladri Sekhar Jyoti <niladri123@gmail.com>
2024-11-24 11:56:17 -0500

    modify App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
+import PostDetail from './components/PostDetail';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={PostList} />
+                <Route path="/create" component={PostForm} />
+                <Route path="/posts/:postId" component={PostDetail} />
+                <Route path="/edit/:postId" component={PostForm} />
+          </Switch>
+        </Router>
+    );
+};
 
 export default App;
